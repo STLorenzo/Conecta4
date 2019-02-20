5a963d3593de0f6940ace7d6b36491c65b3b8713
sergioteso <ssergioteso@gmail.com>
2019-02-20 11:38:33 +0100

    fas

--- app/src/main/java/com/sergioteso/conecta4/GameEditorActivity.kt
+++ app/src/main/java/com/sergioteso/conecta4/GameEditorActivity.kt
@@ -8,6 +8,7 @@ import android.widget.SeekBar
 class GameEditorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_game_editor)
 
         val sb_columns = findViewById<SeekBar>(R.id.seekBar_columns_GameEditor)
         val sb_rows = findViewById<SeekBar>(R.id.seekBar_rows_GameEditor)
